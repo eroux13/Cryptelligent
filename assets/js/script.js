9ecc20b00c2950d06040290d7331911b30887dae
warren clark <wclarkcito@gmail.com>
2021-02-02 19:10:52 -0800

    first ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,69 +30,6 @@ $.ajax({
 
 
 
-    // function handleHistory(value) {
-//     var tempHistory = JSON.parse(window.localStorage.getItem("history"));
-//     console.log(tempHistory)
-//     if (tempHistory === null) {
-//         window.localStorage.setItem("history", JSON.stringify([]));
-//         tempHistory = [];
-//         tempHistory.push({
-//             city: value,
-//             date: new Date().getTime()
-//         });
-//         window.localStorage.setItem("history", JSON.stringify(tempHistory));
 
-
-
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-        //         })
-        //         .then(function (data) {
-        //             console.log(data)
-        //             for (var i = 0; i < data.length; i++) {
-        //                 var createTableRow = document.createElement('tr');
-        //                 var tableData = document.createElement('td');
-        //                 var link = document.createElement('a');
-
-        //                 link.textContent = data[i].html_url;
-        //                 link.href = data[i].html_url;
-
-        //                 tableData.appendChild(link);
-        //                 createTableRow.appendChild(tableData);
-        //                 tableBody.appendChild(createTableRow);
-        //             }
-
-        //         })
-
-        //})
-
-        // var urlRequest = "https://coingecko.p.rapidapi.com/simple/price?ids=%3CREQUIRED%3E&vs_currencies=%3CREQUIRED%3E"
-
-
-        // const settings = {
-        //     "async": true,
-        //     "crossDomain": true,
-        //     "url": urlRequest,
-        //     "method": "GET",
-        //     "headers": {
-        //         "x-rapidapi-key": "e2c463bd64mshff24e0c193a1aa8p1bc812jsn52c41784298e",
-        //         "x-rapidapi-host": "coingecko.p.rapidapi.com"
-        //     }
-        // };
-
-
-        // $.ajax(settings).done(function (response) {
-        //     console.log(response);
-        // });
 
 
