25ff1a5c0bed18c78e54772c2e0bb193ab5f7953
warren clark <wclarkcito@gmail.com>
2021-02-04 21:28:52 -0800

    created lists for market cap coin name trading volume

--- script.js
+++ script.js
@@ -20,15 +20,63 @@ function getApi() {
             console.log(match);
 
             $(match);
+
+
+
+            //Trading Volume
+
+            var tradeVol = data[0].total_volume;
+            var volTag = $("<li>");
+            volTag.addClass("span");
+            //volTag.attr("href", tradeVol);
+            volTag.html(" Trading Volume ");
+
+
+            //name of crypto
+
+            var coinName = data[0].name;
+            var cryptoName = $("<li>");
+            cryptoName.addClass("span");
+            cryptoName.html(coinName);
+
+            //current price
             var searchHistory = data[0].current_price;
             var marketCap = data[0].market_cap;
-            console.log(marketCap)
-            var lowDayPrice = data[0].low_24hr
-            console.log(lowDayPrice);
-            $("#btc").append(searchHistory);
+            var newList = $("<ul>");
+            newList.addClass("form")
+            $("#container").append(newList);
+            var listItem = $("<li>");
+            listItem.addClass("span");
+            listItem.html(" Market Price: " + searchHistory);
+
+
+
+
+            //market cap
+            var newItem2 = $("<li>");
+            newItem2.addClass("form");
+            newItem2.html(" Market Cap: " + marketCap);
+
+
+
+            newList.append(coinName);
+            newList.append(newItem2);
+            newList.append(listItem);
+            console.log(volTag.html)
+            newList.append(tradeVol);
+
+
+
+
+
+
+
+
+
+            //debugger;
 
             //console.log("test3");
-            console.log(searchHistory);
+
 
 
         });
