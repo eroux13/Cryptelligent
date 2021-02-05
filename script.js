d41f1790aa679df711f8d6a2c9c86bd09db74750
EJ <ej.roux13@gmail.com>
2021-02-05 14:20:56 -0800

    added crypto img from dataset

--- script.js
+++ script.js
@@ -1,71 +1,74 @@
 $(document).ready(function () {
 
-var submitCoin = document.querySelector('#magnify')
+    var submitCoin = document.querySelector('#magnify')
 
-function getApi() {
-    var requestURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
-    var searchField = $("#search").val();
+    function getApi() {
+        var requestURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
+        var searchField = $("#search").val();
 
-    console.log(searchField);
-
-
-    $.ajax({
-        url: requestURL,
-        method: 'GET',
-    })
-        .then(function (data) {
-
-            const match = data.find(coin => searchField === coin.symbol)
-            //console.log("test2");
-            console.log(match);
-
-            $(match);
+        console.log(searchField);
 
 
+        $.ajax({
+            url: requestURL,
+            method: 'GET',
+        })
+            .then(function (data) {
 
-            //Trading Volume
+                const match = data.find(coin => searchField === coin.symbol)
+                //console.log("test2");
+                console.log(match);
 
-            var tradeVol = data[0].total_volume;
-            var volTag = $("<li>");
-            volTag.addClass("span");
-            //volTag.attr("href", tradeVol);
-            volTag.html(" Trading Volume ");
-
-
-            //name of crypto
-
-            var coinName = data[0].name;
-            var cryptoName = $("<li>");
-            cryptoName.addClass("span");
-            cryptoName.html(coinName);
-
-            //current price
-            var searchHistory = data[0].current_price;
-            var marketCap = data[0].market_cap;
-            var newList = $("<ul>");
-            newList.addClass("form")
-            $("#container").append(newList);
-            var listItem = $("<li>");
-            listItem.addClass("span");
-            listItem.html(" Market Price: " + searchHistory);
+                $(match);
 
 
 
+                //Trading Volume
 
-            //market cap
-            var newItem2 = $("<li>");
-            newItem2.addClass("form");
-            newItem2.html(" Market Cap: " + marketCap);
-
-
-
-            newList.append(coinName);
-            newList.append(newItem2);
-            newList.append(listItem);
-            console.log(volTag.html)
-            newList.append(tradeVol);
+                var tradeVol = data[0].total_volume;
+                var volTag = $("<li>");
+                volTag.addClass("span");
+                //volTag.attr("href", tradeVol);
+                volTag.html(" Trading Volume: $" + tradeVol);
 
 
+                //name of crypto
+
+                var coinName = data[0].name;
+                var cryptoName = $("<li>");
+                cryptoName.addClass("span");
+                cryptoName.html(coinName);
+
+                //current price
+                var searchHistory = data[0].current_price;
+                var marketCap = data[0].market_cap;
+                var newList = $("<ul>");
+                newList.addClass("form")
+                $("#container").append(newList);
+                var listItem = $("<li>");
+                listItem.addClass("span");
+                listItem.html(" Market Price: $" + searchHistory);
+
+                // crypto img
+                var cryptoImg = data[0].image;
+                var cryptoImgItem = $("<img>");
+                cryptoImgItem.attr("src", cryptoImg);
+                cryptoImgItem.attr("id", "cryptoImg");
+                cryptoImgItem.addClass("responsive-img");
+
+
+                //market cap
+                var newItem2 = $("<li>");
+                newItem2.addClass("form");
+                newItem2.html(" Market Cap: $" + marketCap);
+
+
+                $("#container").prepend(cryptoImgItem);
+                newList.append(coinName);
+                newList.append(newItem2);
+                newList.append(listItem);
+                console.log(volTag.html)
+                newList.append(volTag);
 
 
 
@@ -73,22 +76,24 @@ function getApi() {
 
 
 
-            //debugger;
-
-            //console.log("test3");
 
 
+                //debugger;
 
-        });
-};
+                //console.log("test3");
 
-submitCoin.addEventListener('click', getApi);
 
-// submitCoin.on('click', function (event) {
-//     console.log(event);
-//     event.preventDefault();
-// });
-  
+
+            });
+    };
+
+    submitCoin.addEventListener('click', getApi);
+
+    // submitCoin.on('click', function (event) {
+    //     console.log(event);
+    //     event.preventDefault();
+    // });
+
     // Bloomberg News API
     const settings = {
         "async": true,
